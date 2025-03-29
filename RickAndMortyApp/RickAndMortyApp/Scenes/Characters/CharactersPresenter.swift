5e2b3a0570a1cb9d92b2cc48d9cb99bdad21fb9c
VadimSorokolit <macintosh@ukr.net>
2025-03-29 10:30:13 +0200

    Implement CharactersInteractor and CharactersPresenter

--- RickAndMortyApp/RickAndMortyApp/Scenes/Characters/CharactersPresenter.swift
+++ RickAndMortyApp/RickAndMortyApp/Scenes/Characters/CharactersPresenter.swift
@@ -6,3 +6,23 @@
 //
     
 
+import UIKit
+
+protocol CharactersPresenterLogic {
+    func presentCharacters(response: CharacterModels.DisplayCharacters.Response)
+    func presentError(error: Error)
+}
+
+class CharactersPresenter: CharactersPresenterLogic {
+    
+    func presentCharacters(response: CharacterModels.DisplayCharacters.Response) {
+        let cartoonCharacters = response.characters
+    }
+    
+    func presentError(error: Error) {
+        let errorMessage = error.localizedDescription
+        
+        let errorViewModel = CharacterModels.DisplayCharacters.ViewModel(characterInformation: [], errorMessage: errorMessage)
+    }
+    
+}
